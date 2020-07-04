495936f16690903098c958621685ec17e045ac46
DramaLvl1 <53279807+DramaLvl1@users.noreply.github.com>
2020-07-04 22:07:26 +0200

    Update Main.php
    ...

--- src/SimpleGM/Drama_Lvl1/Main.php
+++ src/SimpleGM/Drama_Lvl1/Main.php
@@ -24,7 +24,7 @@ class Main extends PluginBase{
                         if($args[0] === "0"){
                             $player->setGamemode(0);
                             $player->sendMessage("§8[§bSimple§3GM§8] §aDein Gamemode wurde auf §2Überleben §agesetzt");
-                            $player->sendMessage("§8[§bSimple§3GM§8] §aDu hast den Gamemode von §2" . $change . " §azu §2Überleben §ageändert");
+                            $sender->sendMessage("§8[§bSimple§3GM§8] §aDu hast den Gamemode von §2" . $change . " §azu §2Überleben §ageändert");
                         } else if($args[0] === "1"){
                             $player->setGamemode(1);
                             $player->sendMessage("§8[§bSimple§3GM§8] §aDein Gamemode wurde auf §2Kreativ §agesetzt");
